ae6fcdb6c32996b80a1f1bfa50c3d72fac132607
vishal <vishallengare61@gmail.com>
2025-07-08 15:43:28 +0530

    Added vercel.json and deployment configs

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-
-
-
 import express from "express";
+import cors from "cors";
+
 const app = express();
-app.listen(8080)
+const PORT =  8080;
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+// Middleware
+app.use(cors()); // allow all domains
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.get('/hello', (req, res)=>{
-    res.status(200).json("Hello from Server")
-})
+// Simple route
+app.get('/hello', (req, res) => {
+    res.status(200).json("Hello from Server");
+});
 
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
