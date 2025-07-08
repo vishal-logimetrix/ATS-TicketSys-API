6d9ed7a0a32f937ea4b29b5e1d3c4feeac57fbef
vishal <vishallengare61@gmail.com>
2025-07-08 15:57:28 +0530

    testing env variables on vercel

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 
 const app = express();
-const PORT =  8080;
+const PORT =  process.env.PORT || 8080;
 
 // Middleware
 app.use(cors()); // allow all domains
